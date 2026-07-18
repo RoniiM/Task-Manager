72209dc17163898c0e4029286a75ccf205793fd8
RoniiM <234032619+RoniiM@users.noreply.github.com>
2026-07-18 11:04:39 +0300

    configure additional routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register';
 import { LoginComponent } from './login/login';
+import { TasksComponent } from './tasks/tasks';
+import { TaskformComponent } from './taskform/taskform';
 
 
 export const routes: Routes = [
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'tasks', component: TasksComponent},
+    {path: 'tasks/add', component: TaskformComponent},
+    {path: 'tasks/edit/:id', component: TaskformComponent},
 
     // DEFAULT FALLBACK URL
     {path: '', redirectTo: '/tasks', pathMatch: 'full'},
