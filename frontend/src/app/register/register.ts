cb7883f3cd892fc386d9925b79a721c26bc03855
RoniiM <234032619+RoniiM@users.noreply.github.com>
2026-07-18 10:34:40 +0300

    configure register component

--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -1,9 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
-export class Register {}
+export class RegisterComponent {
+
+  regForm: FormGroup;
+  error: string = "";
+
+  constructor(private fb:FormBuilder, 
+    private apiService: Api,
+  private router: Router ){
+
+    this.regForm = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if (this.regForm.invalid) {
+      this.error = "please fill in all field";
+      return;
+    }
+    this.error = "";
+
+    this.apiService.registerUser(this.regForm.value).subscribe({
+      next: (res:any) =>{
+        if (res.statusCode === 200) {
+          this.router.navigate(['/login']);
+        }else{
+          this.error = res.message || "Register not succesful" 
+        }
+      },
+      error:(error: any) =>{
+        this.error = error.error?.message || error.message
+      }
+    })
+  }
+
+}
