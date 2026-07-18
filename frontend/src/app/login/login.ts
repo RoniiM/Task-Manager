e83b33924338ff3388856fc6dd85524cb565bfed
RoniiM <234032619+RoniiM@users.noreply.github.com>
2026-07-18 10:49:07 +0300

    configure login component

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class LoginComponent {
+
+  loginForm: FormGroup;
+  error: string = "";
+
+  constructor(private fb: FormBuilder,
+    private apiService: Api,
+    private router: Router) {
+
+    this.loginForm = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.error = "please fill in all field";
+      return;
+    }
+    this.error = "";
+
+    this.apiService.loginUser(this.loginForm.value).subscribe({
+      next: (res: any) => {
+        if (res.statusCode === 200) {
+          this.apiService.saveToken(res.data)
+          this.router.navigate(['/tasks']);
+        } else {
+          this.error = res.message || "Login not succesful"
+        }
+      },
+      error: (error: any) => {
+        this.error = error.error?.message || error.message
+      }
+    })
+  }
+}
