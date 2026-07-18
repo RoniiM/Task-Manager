bad5efd2826132e41a6cdd84d7825805e9601cf0
RoniiM <234032619+RoniiM@users.noreply.github.com>
2026-07-18 10:49:19 +0300

    add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register';
+import { LoginComponent } from './login/login';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+
+    // DEFAULT FALLBACK URL
+    {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+    {path: '**', redirectTo: '/tasks', pathMatch: 'full'}
+];
