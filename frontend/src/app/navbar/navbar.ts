b63abbab450138e44757ff142206746458d9b272
RoniiM <234032619+RoniiM@users.noreply.github.com>
2026-07-18 10:22:10 +0300

    configure navbar & footer

--- frontend/src/app/navbar/navbar.ts
+++ frontend/src/app/navbar/navbar.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { booleanAttribute, Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+
+constructor(private readonly apiService: Api, private router: Router){}
+
+get isAuthenticated(): boolean
+  {
+    return this.apiService.isAuthenticated();
+  }
+
+  handleLogout(): void
+  {
+    const isLogout = window.confirm("Are you sure you want to logout?");
+
+    if(isLogout)
+    {
+      this.apiService.logout();
+      this.router.navigate(['/login']);
+    }
+  }
+
+}
